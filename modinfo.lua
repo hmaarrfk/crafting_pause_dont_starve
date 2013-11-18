344198cfae4bd2353443b0f7172a1493aae7bf78
Mark Harfouche <mark.harfouche@gmail.com>
2013-11-17 20:13:09 -0800

    Updated to work with the mouse as well.

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
-name = "Crafting Pause"
+name = "Crafting Pause Staging"
 description = "Stops time when you open the crafting tab through CapsLock (or a controller I guess)."
 author = "hmaarrfk"
-version = 1.0
+version = 1.1
 forumthread = ""
 api_version = 4
--- modmain.lua
+++ modmain.lua
@@ -5,24 +5,63 @@
 --]]
 
 --local require = GLOBAL.require
---require "mainfunctions"
 
+local function MyControllerCrafting(self)
+  local oldClose                   = self.Close
+  local oldOpenRecipeTab           = self.OpenRecipeTab
 
--- I learned how to modify functions from 
--- RPG HUD' CTabs function
-local function MyCraftTabs(self)
-  local oldOpenControllerCrafting = self.OpenControllerCrafting
-  local oldCloseControllerCrafting = self.CloseControllerCrafting
-
-  function self:OpenControllerCrafting()
-    oldOpenControllerCrafting(self)
-    GLOBAL.SetPause(true, "Controller Crafting")
+  function self:Close(fn)
+    oldClose(self, fn)
+    GLOBAL.SetPause(false)
   end
 
-  function self:CloseControllerCrafting()
-    oldCloseControllerCrafting(self)
-    GLOBAL.SetPause(false)
+  -- You can't simply call pause in Open
+  -- because when you scroll down, it actually calls Crafting.Close
+  -- Which will unpause the game.
+  -- So you do it in OpenRecipeTab, that way, it will pause the game 
+  -- again RIGHT after it is unpaused
+  function self:OpenRecipeTab(idx)
+    local tab = oldOpenRecipeTab(self, idx)
+    GLOBAL.SetPause(true)
+    return tab
   end
 end
 
-AddClassPostConstruct("widgets/crafttabs", MyCraftTabs)
+AddClassPostConstruct("widgets/controllercrafting", MyControllerCrafting)
+
+
+
+
+
+local function MyCrafting(self, num_slots)
+  local oldOpen = self.Open
+  local oldClose = self.Close
+
+  local oldScrollUp = self.ScrollUp
+  local oldScrollDown = self.ScrollDown
+
+  function self:Open(fn)
+    oldOpen(self, fn)
+    GLOBAL.SetPause(true, "Menu Crafting")
+  end
+  function self:Close(fn)
+    oldClose(self, fn)
+    GLOBAL.SetPause(false)
+  end
+
+  -- For some reason, the code checks if the state of the game is paused during these.
+  -- Instead of changing the core of the functions (probably be broken by updates)
+  -- We do this instead
+  function self:ScrollUp()
+    GLOBAL.SetPause(false)
+    oldScrollUp(self)
+    GLOBAL.SetPause(true)
+  end
+  function self:ScrollDown()
+    GLOBAL.SetPause(false)
+    oldScrollDown(self)
+    GLOBAL.SetPause(true)
+  end
+end
+
+AddClassPostConstruct("widgets/crafting", MyCrafting)
