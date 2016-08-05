297a4a5c83382b3aad64c57c03af524b3f78300b
Mark Harfouche <mark.harfouche@gmail.com>
2016-08-04 21:53:14 -0700

    now shipwrecked compatible

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,7 @@
 name = "Crafting Pause Staging"
 description = "Stops time when you open the crafting tab through CapsLock (or a controller I guess)."
 author = "hmaarrfk"
-version = 1.1
+version = 1.2
 forumthread = ""
 api_version = 4
+shipwrecked_compatible = true
