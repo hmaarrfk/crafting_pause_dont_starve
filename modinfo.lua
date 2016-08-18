9efda29bf42412862794c954422779f34f03ee0c
Mark Harfouche <mark.harfouche@gmail.com>
2016-08-17 18:56:57 -0700

    Updated the API version. No more out of date

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,10 @@
 name = "Crafting Pause Staging"
 description = "Stops time when you open the crafting tab through CapsLock (or a controller I guess)."
 author = "hmaarrfk"
-version = 1.2
+version = 1.4
 forumthread = ""
-api_version = 4
+api_version = 6
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
 shipwrecked_compatible = true
