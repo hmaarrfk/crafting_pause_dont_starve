b499457b353e18dcb9e30281ba3a4b9818cd86d1
Mark Harfouche <mark.harfouche@gmail.com>
2016-09-19 23:14:31 -0700

    Now added a modicon. Changed the name to remove "staging"

--- modinfo.lua
+++ modinfo.lua
@@ -1,10 +1,14 @@
-name = "Crafting Pause Staging"
-description = "Stops time when you open the crafting tab through CapsLock (or a controller I guess)."
+name = "Crafting Pause"
+description = "Stops time when you open the crafting tab through CapsLock or a controller."
 author = "hmaarrfk"
-version = 1.4
+version = 1.5
 forumthread = ""
 api_version = 6
 
 dont_starve_compatible = true
 reign_of_giants_compatible = true
 shipwrecked_compatible = true
+
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
